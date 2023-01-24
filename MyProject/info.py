2332b7b5bbeb3bf30a44aa66a0161d653163b4c6
Ashiah kumar <ashish.dubey8789@gmail.com>
2023-01-24 14:46:23 +0530

    first step completed

---
+++ MyProject/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ashish_kumar3.scsebtech@galgotiasuniversity.edu.in'
+EMAIL_HOST_PASSWORD = 'Galgotias@321'
+EMAIL_PORT = 587
---
+++ Myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('forget-password/',views.user_change_pass, name='changepass'),
+]
